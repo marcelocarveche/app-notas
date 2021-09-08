b76a78f4fbdcecc337d15209a7382af369d1e96d
Marcelo Carveche <marcelo.carveche@estantemagica.com.br>
2021-09-08 15:15:31 -0400

    Projeto app de tarefas finalizado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Projeto Inicial</h1>
-    </div>
-  );
-}
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
 
-export default App;
+export default class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      notas: [],
+    };
+  }
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayDeNotas = [...this.state.notas, novaNota]
+    const novoEstado= {
+      notas: novoArrayDeNotas,
+    };
+    this.setState(novoEstado);
+    // console.log(this.notas.length);
+    // console.log('Uma nova nota foi criada Título: ' + titulo + ' Texto: ' + texto);
+  }
+    render(){
+      return (
+        <section>
+          <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+          <ListaDeNotas notas={this.state.notas}/>
+        </section>
+      );
+    }
+}
---
+++ src/components/FormularioCadastro/index.jsx
@@ -0,0 +1,50 @@
+import react, { Component } from "react";
+import './estilos.css';
+export default class FormularioCadastro extends Component {
+  constructor(props){
+    super(props);
+    this.titulo="";
+    this.texto="";
+  }
+
+  _handleMudancaDeTitulo(evento){
+    evento.stopPropagation();
+    this.titulo = evento.target.value;
+  }
+
+  _handleMudancaDoTexto(evento){
+    evento.stopPropagation();
+    this.texto = evento.target.value;
+  }
+
+  _criarNota(evento){
+    evento.preventDefault();
+    evento.stopPropagation();
+    this.props.criarNota(this.titulo, this.texto);
+  }
+
+
+  render() {
+    return (
+      <form className="form-cadastro "
+        onSubmit={this._criarNota.bind(this)}
+      >
+        <input
+          type="text"
+          placeholder="Título"
+          className="form-cadastro_input"
+          onChange={this._handleMudancaDeTitulo.bind(this)}
+        />
+        <textarea
+          rows={15}
+          placeholder="Escreva sua nota..."
+          className="form-cadastro_input"
+          onChange={this._handleMudancaDoTexto.bind(this)}
+        />
+        <button className="form-cadastro_input form-cadastro_submit">
+          Criar Nota
+        </button>
+      </form>
+    );
+  }
+}
